37086094206a598683dc9f649c16ceae7ec5c6b9
branch <dd.com>
2023-12-22 17:51:26 +0900

    [박규현]

--- final/server/appForder/festivalApp/festivalapp.js
+++ final/server/appForder/festivalApp/festivalapp.js
@@ -3,12 +3,22 @@ const router = express.Router();
 const mysql = require('../../db.js');
 
 
+// 축제 전체리스트
 router.get('/', async (req, res) => {
     let list = await mysql.query('fesList');
     res.send(list);
     
 });
 
+// 축제 등록
+router.post('/', async (req, res) => {
+    let data = req.body.param;
+    let result = await mysql.query('fesInsert', data);
+    res.send(result);
+})
+// 축제 수정
+
+// 축제 삭제
 
 
 module.exports = router;
--- final/server/db/festivalSql/festivalsql.js
+++ final/server/db/festivalSql/festivalsql.js
@@ -1,8 +1,19 @@
 module.exports = {
+    
     fesList : `SELECT p.*, COUNT(c.BNO) AS comment_count
     FROM T_BOARD_BOARD p
     LEFT JOIN T_COMMENT_BOARD c ON p.NO = c.BNO
     GROUP BY p.NO`,
 
+    // 등록
+    fesInsert : `insert into festival set
+            f_code = ?, f_category, f_reg, f_name, f_number, f_loc,
+            f_firstday, f_lastday, f_content, f_price, f_url`,
+
+    // 수정
+    fesUpdate : `update festival set ? where f_code`,
+
+    // 삭제
+    fesDelete : `delete * from festival where f_code`
     
 }
