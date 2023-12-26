a87c3bf65b318f6517940baf852f028a762657ef
SungohYoon <nsa30003@gmail.com>
2023-12-26 17:53:02 +0900

    [윤성오] 회원가입 폼 구현

--- final/client/src/views/Main.vue
+++ final/client/src/views/Main.vue
@@ -2,22 +2,19 @@
   <div>
     <Logo></Logo>
     <Header></Header>
+    <router-view />
     <Footer></Footer>
   </div>
 </template>
 
-
 <script>
-  import Logo from '../layouts/MainLogo.vue'
-  import Header from '../layouts/MainHeader.vue';
-  import Footer from '../layouts/MainFooter.vue';
+import Logo from '../layouts/MainLogo.vue';
+import Header from '../layouts/MainHeader.vue';
+import Footer from '../layouts/MainFooter.vue';
 
-  export default {
-    components  : { Logo, Header, Footer }
-  }
+export default {
+  components: { Logo, Header, Footer },
+};
 </script>
 
-
-<style>
-
-</style>
+<style></style>
