9dbcb0d2804a5bc329d0a3045c2dd3defb43f6af
현진 <148922882+KangHyunJin2@users.noreply.github.com>
2023-12-26 11:01:19 +0900

    [강현진] 쿼리문 짜기

--- final/server/db/productSql/productsql.js
+++ final/server/db/productSql/productsql.js
@@ -1,7 +1,16 @@
-// 강현진
+// 강현진 상품 CRUD
 module.exports = {
     productList : `SELECT p.*, COUNT(c.BNO) AS comment_count
                     FROM T_BOARD_BOARD p
                     LEFT JOIN T_COMMENT_BOARD c ON p.NO = c.BNO
                     GROUP BY p.NO`,
+    // 등록
+    productInsert : `INSERT INTO product SET
+                     prod_code, prod_name, prod_content, prod_price, prod_count, prod_loc, prod_cate`,
+
+    //수정
+    productUpdate : `UPDATE product SET ? WHERE prod_code = ? `,
+
+    //삭제
+    productDelete : `DELETE * FROM product WHERE prod_code = ? `,
 }
