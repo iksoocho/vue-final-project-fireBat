d72fbfcd634a3a06bfc14f92a2f1d351e7990e31
윤성오 <nsa30003@gmail.com>
2023-12-28 01:07:18 +0900

    [윤성오] 충돌해결

--- final/server/db/festivalSql/festivalsql.js
+++ final/server/db/festivalSql/festivalsql.js
@@ -1,20 +1,18 @@
 module.exports = {
     
-<<<<<<< HEAD
-    // 전체리스트
-    fesList : `select * from festival`,
+
 
     // 상세페이지 개별조회
     fesInfo : `select f_code, f_category, f_reg, f_name, f_number, f_loc
-    FROM t_board_board
+    FROM festival
     WHERE f_code = ?`,
-=======
+
     // 전체 리스트 조회
     fesList : `select * from festival`,
 
     // 단건 조회
     fesInfo : `select f_code from festival where f_name = ?`,
->>>>>>> c14076d5b5784099c8570f6c05c94484365c370b
+
 
     // 등록
     fesInsert : `insert into festival set ?`,
