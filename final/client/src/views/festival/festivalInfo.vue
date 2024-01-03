f506356a370a4d375ca52d27368dd6dacc7d57f7
branch <dd.com>
2024-01-03 10:19:05 +0900

    [박규현]

--- final/client/src/views/festival/festivalInfo.vue
+++ final/client/src/views/festival/festivalInfo.vue
@@ -65,12 +65,11 @@ export default {
 
     },
 
-    
-
     created() {
     this.searchNo = this.$route.query.f_code;   // 페이지요청은 router  페이지가 열릴때는 route
     this.getFesInfo();
-},
+    
+    },
     methods : {
 
         async getFesInfo() {
@@ -86,12 +85,15 @@ export default {
                 }
             
         },
+
         goFesUpdate(f_code){
                 this.$router.push({path : '/festivalUpdate', query:{f_code : f_code}})
-    },
-    async fesDelete(f_code){
+
+        },
+
+        async fesDelete(f_code){
                 let result = await axios.delete(`/api/festival/delete/${f_code}`)
-                                      .catch(err=>console.log(err));
+                                        .catch(err=>console.log(err));
                 console.log(result.data);
                 let count = result.data.affectedRows;   
                 if(count == 0){
@@ -100,63 +102,63 @@ export default {
                     alert('정상적으로 삭제 되었습니다.')
                     this.$router.push({name : 'festivalList'});
                 }
-            },
+        },
+
             getDateFormat(date){
             return this.$dateFormat(date);   // 날짜 변환
+
         },
         
         // 카카오맵 API 지도
         initializeMap() {
             // ID가 'map'인 HTML 요소를 검색하여 변수에 할당 mapContainer.
-           const mapContainer = document.getElementById('map');
+            const mapContainer = document.getElementById('map');
            // 위도 경도 기본 초기값
-           const mapOption = {
-              center: new kakao.maps.LatLng(33.450701, 126.570667),
-              level: 5,
-           };
+            const mapOption = {
+                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                level: 5,
+            };
            // 지정된 컨테이너랑 옵션 사용해서 새로운 카카오맵 API지도 객체 생성
-           const map = new kakao.maps.Map(mapContainer, mapOption);
+            const map = new kakao.maps.Map(mapContainer, mapOption);
            // 축제 장소의 주소를 위도 경도가 아닌 좌표로 변환하려고 지오코더 객체 만듬
-           const geocoder = new kakao.maps.services.Geocoder();
-           const address = this.fesInfo.f_loc;
+            const geocoder = new kakao.maps.services.Geocoder();
+            const address = this.fesInfo.f_loc;
            // 지오코더 이용해서 주소검색
-           geocoder.addressSearch(address, (result, status) => {
-              if (status === kakao.maps.services.Status.OK) {
-                 const coords = new kakao.maps.LatLng(result[0].y, result[0].x);  // 지오코딩 조건문 값을 위도 경도 좌표 출력
+            geocoder.addressSearch(address, (result, status) => {
+                if (status === kakao.maps.services.Status.OK) {
+                const coords = new kakao.maps.LatLng(result[0].y, result[0].x);  // 지오코딩 조건문 값을 위도 경도 좌표 출력
                 // 지정된 좌표에 마커 표시
-                 const marker = new kakao.maps.Marker({
+                const marker = new kakao.maps.Marker({
                     map: map,
                     position: coords,
-                 });
+                });
                  // 좌표에 데이터(축제이름) 받아와서 마커에 표시
-                 const infowindow = new kakao.maps.InfoWindow({
+                const infowindow = new kakao.maps.InfoWindow({
                     content: `<div style="width:150px;text-align:center;padding:6px 0;">${this.fesInfo.f_name}</div>`,
-                 });
+                });
                  // 지도의 마커 위치에 정보창을 열고 지도 중심을 축제 장소 좌표로 설정합니다
-                 infowindow.open(map, marker);
-  
-                 map.setCenter(coords);
-              }
-           });
+                infowindow.open(map, marker);
+
+                map.setCenter(coords);
+            }
+        });
         },
     },
-  
-    
-  }
-  
-  </script>
-  
-  <style scoped>
-  #map {
-  width: 1300px;
-  height: 400px;
-  }
-  
-  .button-group {
-  margin: 10px 0px;
-  }
-  
-  button {
-  margin: 0 3px;
-  }
-  </style>
+}
+
+    </script>
+
+    <style scoped>
+        #map {
+            width: 1300px;
+            height: 400px;
+        }
+
+        .button-group {
+            margin: 10px 0px;
+        }
+
+        button {
+            margin: 0 3px;
+        }
+    </style>
