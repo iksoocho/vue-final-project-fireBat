9c3ec1b1e126042f159e1ca21f22e3bfacdc1478
윤성오 <nsa30003@gmail.com>
2024-01-03 20:50:43 +0900

    [윤성오] 마이페이지

--- final/client/src/views/member/MyPage.vue
+++ final/client/src/views/member/MyPage.vue
@@ -1,38 +1,70 @@
 <template>
-  <h1>마이페이지입니다.</h1>
+  <div>
+    <h3>회원 정보 수정</h3>
+    <form @submit.prevent="submitForm">
+      <div>
+        <label>아이디</label><br>
+        <input type="text" v-model="user.user_id" readonly />
+      </div>
+
+      <div>
+        <label>새로운 비밀번호</label><br>
+        <input type="password" v-model="user.user_pw" />
+      </div>
+
+      <div>
+        <label>기존 비밀번호 확인</label><br>
+        <input type="password" v-model="user.user_recpw" />
+      </div>
+
+      <!-- 기타 필드들도 유사하게 추가 -->
+
+      <button type="submit">정보 수정</button>
+    </form>
+  </div>
 </template>
+
 <script>
 import axios from 'axios';
 
 export default {
-  data() {
-    return {
-      userInfo: {},
-      user_no: ''
-    }
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
   },
-  methods : {
-    async userInfo(){
-      let data = {
-        param : {
-          user_id: '',
-          user_pw: '',
-          user_recpw: '',
-          user_name: '',
-          user_birth:'',
-          user_tel: '',
-          user_email: '',
-          user_zip: '',
-          user_addr: '',
-          user_detail_addr: '',
-          user_receive_email: '',
-          user_regdate: '',
-          user_status: '',
-          user_deldate: '',
-          user_gender: '',
+  methods: {
+    async submitForm() {
+      try {
+        if (this.user.user_pw !== this.user.user_recpw) {
+          window.alert('새로운 비밀번호와 기존 비밀번호 확인이 일치하지 않습니다.');
+          return;
         }
+
+        let data = {
+          user_id: this.user.user_id,
+          user_pw: this.user.user_pw,
+          // 기타 필요한 정보들도 추가
+        };
+
+        let result = await axios.put(`/api/user/update/${this.user.user_id}`, data, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        console.log(result.data);
+
+        if (result.data.affectedRows === 1) {
+          window.alert('회원 정보가 수정되었습니다.');
+        } else {
+          window.alert('회원 정보 수정에 실패했습니다. 다시 시도해주세요.');
+        }
+      } catch (error) {
+        console.error(error);
+        window.alert('회원 정보 수정에 실패했습니다. 다시 시도해주세요.');
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
--- final/server/db/memberSql/membersql.js
+++ final/server/db/memberSql/membersql.js
@@ -3,7 +3,7 @@ module.exports = {
   userList: `SELECT * FROM user`, // 회원리스트
   userIdInfo: `SELECT user_id FROM user WHERE user_tel = ?`, // 아이디 찾기(회원정보에 등록한 휴대전화로 인증)
   userDelete: `DELETE FROM user WHERE user_no = ?`, // 회원정보삭제
-  userUpdate: `UPDATE user SET ? WHERE user_no = ?`, // 회원정보수정
+  userUpdate: `UPDATE user SET ? WHERE user_id = ?`, // 회원정보수정
   userCheck: `SELECT COUNT(*) AS count FROM user WHERE user_id = ?`, // 아이디 중복 체크
   userLogin: `SELECT * FROM user WHERE user_id = ? AND user_pw = ?` // 로그인
 };
