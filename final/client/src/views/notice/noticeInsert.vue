96db4433090854b9be0b3c81c5e7c86516c80755
iksoocho <jogaeya@gmail.com>
2024-01-04 11:19:39 +0900

    [조익수] ㅈ같네

--- final/client/src/views/notice/noticeInsert.vue
+++ final/client/src/views/notice/noticeInsert.vue
@@ -6,23 +6,16 @@
     </div>
 	
 		<table id="writetable" >
+			<tr><td class="title"><p>제목</p></td><td><input v-model="noticeInfo.notice_title" type="text" id="title" name="title" required 
+				></td></tr>
+			<tr><td class="title"><p>첨부</p></td><td><input type="file" id="avatar" name="file"></td></tr>
+			<tr><td colspan="2" id="textarea"><textarea id="textarea2" cols="130" rows="15" name="content" v-model="noticeInfo.notice_content"></textarea></td></tr>
 			
-				<tr><td class="title"><p>작성자</p></td><td><span>{{ this.$store.getters.userName }}</span></td></tr>
-				<tr><td class="title"><p>카테고리</p></td><td><select id="select" name="category" v-model="qnaInfo.qna_category">
-							<option value="1">배송 문의</option>
-							<option value="2">축제 문의</option>
-							<option value="3">기타 문의</option>
-						</select></td></tr>
-				<tr><td class="title"><p>제목</p></td><td><input v-model="qnaInfo.qna_title" type="text" id="title" name="title" required 
-					></td></tr>
-				<tr><td class="title"><p>첨부</p></td><td><input type="file" id="avatar" name="file"></td></tr>
-				<tr><td colspan="2" id="textarea"><textarea id="textarea2" cols="130" rows="15" name="content" v-model="qnaInfo.qna_content"></textarea></td></tr>
-				
 		</table>
 		<div style="text-align: center;">
-					<button type="button" class="btn btn-outline-danger me-2 mt-2" @click="insertInfo">작성</button>
-					<button type="reset" class="btn btn-danger mt-2">취소</button>
-				</div>
+			<button type="button" class="btn btn-outline-danger me-2 mt-2" @click="insertNotice">작성</button>
+			<button type="reset" class="btn btn-danger mt-2">취소</button>
+		</div>
 		
 		
 	</div>
@@ -35,21 +28,15 @@ import Swal from 'sweetalert2';
 export default {
 	data(){
 		return {
-			qnaInfo:{
-				qna_title:'',
-				qna_content:'',
-				user_no:0,
-				qna_category:0,
+			noticeInfo:{
+				notice_title:'',
+				notice_content:'',
+			
 			},
-			userNo:0
+			
 		}
 	},
 	created(){
-		
-		this.getUserNo();
-		
-		console.log('user_no : ',this.userNo)
-		// this.qnaInfo.user_no = this.userNo;
 		
 	},
 	methods:{
@@ -61,28 +48,21 @@ export default {
             
             return `${year}-${month}-${day}`
         },
-		async insertInfo(){
-			let info = this.qnaInfo;  
-         	 info.user_no = this.userNo;
+		async insertNotice(){
+			
             if(!this.validation()) return;       //아예 아래 전체를 감싸도 됨
 
             let data = {
-                param : this.qnaInfo                    //"param" : 형태로 보내야됨
+                param : this.noticeInfo                    //"param" : 형태로 보내야됨
             };
             
-            let result = await axios(`/api/qna`,  {              //axios.post(경로,  data)도 가능하지만 초보일땐 왼쪽처럼 하자
-                method : 'post',
-                headers : {
-                    'Content-Type' : 'application/json'
-                },
-                data : JSON.stringify(data)
-            })
-            .catch(err=>console.log(err))    
+            let result = await axios.post("/api/notice", data)
+                               .catch(err => console.log(err));   
 
-            console.log(result.data);    
+               
             if(result.data.message == ''){
                 Swal.fire(`정상적으로 등록 되었습니다.`)
-                this.$router.push({ path: '/qnaList' });
+                this.$router.push({ path: '/noticeList' });
             }else{
                 Swal.fire(`등록 실패.`)
                 
@@ -91,23 +71,18 @@ export default {
 
         },
 		validation(){
-            if(this.qnaInfo.qna_title == ''){
-                alert('사원번호가 입력되지 않았습니다.')
+            if(this.noticeInfo.notice_title == ''){
+                Swal.fire(`제목이 입력되지 않았습니다.`)
                 return false;
             }
-            if(this.qnaInfo.qna_content== ''){
-                alert('부서가 입력되지 않았습니다.')
+            if(this.noticeInfo.notice_content== ''){
+                Swal.fire(`내용이 입력되지 않았습니다.`)
                 return false;
             }
 
             return true;
         },
-		async getUserNo(){
-             this.userNo = ((await axios.get(`/api/qna/userNo/${this.$store.getters.userName}`).catch(err=>console.log(err))).data).user_no
-			 console.log(this.userNo)
-			return this.userNo;
-			
-        }
+		
 	}
 }
 </script>
--- final/server/app.js
+++ final/server/app.js
@@ -5,8 +5,9 @@ const qnaRouter = require('./appForder/qnaApp/qnaapp.js');
 const userRouter = require('./appForder/memberApp/memberapp.js');
 const fesRouter = require('./appForder/festivalApp/festivalapp.js');
 const productRouter = require('./appForder/productApp/productapp.js');
+const noticeRouter = require('./appForder/noticeApp/noticeapp.js');
 const paymentRouter = require('./appForder/paymentApp/paymentapp.js');
-const noticeRouter = require('./appForder/noticeApp/noticeapp.js')
+
 
 
 app.use(
@@ -23,5 +24,5 @@ app.use('/qna', qnaRouter);
 app.use('/user', userRouter);
 app.use('/festival', fesRouter);
 app.use('/product', productRouter);
-app.use('/notice', noticeRouter)
+app.use('/notice', noticeRouter);
 app.use('/pay', paymentRouter);
