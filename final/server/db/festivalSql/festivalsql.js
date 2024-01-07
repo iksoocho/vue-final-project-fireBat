cf4f1588520e7bf2a5c137d6591c16622be3c968
parkhyunn <148922933+parkhyunn@users.noreply.github.com>
2024-01-07 20:24:28 +0900

    [박규현] 01-07

--- final/server/db/festivalSql/festivalsql.js
+++ final/server/db/festivalSql/festivalsql.js
@@ -20,13 +20,18 @@ module.exports = {
     fesRandomList : `select * from festival order by rand() limit 6`,
 
     // 달력 일별 조회
-    fesCalList : `select * from festival where ? between f_firstday and f_lastday;`,
-
-
-    fesSearch : `select * from festival where f_name like concat(concat('%',?),'%')`,
+    fesCalList : `select * from festival where ? between f_firstday and f_lastday`,
 
     // 축제목록 검색 조회
-    fesSearch : `select * from festival where f_name like concat(concat('%',?),'%') `,
+    fesSearch : `select * from festival where f_code LIKE CONCAT(CONCAT('%',?),'%')
+                                            or f_category LIKE CONCAT(CONCAT('%',?),'%')
+                                            or f_reg LIKE CONCAT(CONCAT('%',?),'%')
+                                            or f_name LIKE CONCAT(CONCAT('%',?),'%')
+                                            or f_number LIKE CONCAT(CONCAT('%',?),'%')
+                                            or f_firstday LIKE CONCAT(CONCAT('%',?),'%')
+                                            or f_lastday LIKE CONCAT(CONCAT('%',?),'%')
+                                            or f_price LIKE CONCAT(CONCAT('%',?),'%')
+                                            or f_url LIKE CONCAT(CONCAT('%',?),'%')`,
 
     fesImgInsert: `insert into f_imgs set f_code = ?, f_filename = ?`,
 
@@ -35,5 +40,6 @@ module.exports = {
     fesImgSelect : `select * from f_imgs where f_code = ?`,
 
     fesCheckCode : `select count(*) as count from festival where f_code = ?`,
-    
+
+
 }
