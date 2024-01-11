f08d0bf2006e47515b57953474aebaaa6086a890
iksoocho <jogaeya@gmail.com>
2024-01-11 13:49:14 +0900

    [조익수]

--- final/client/src/layouts/MainProductCard.vue
+++ final/client/src/layouts/MainProductCard.vue
@@ -20,19 +20,12 @@
                     </div>
                          <div class="place_info">
                             <a href="destination_details.html"><h3>{{pro.prod_name}}</h3></a>
-                            <p>United State of America</p>
+                            
                             <div class="rating_days d-flex justify-content-between">
-                                <span class="d-flex justify-content-center align-items-center">
-                                     <i class="fa fa-star"></i> 
-                                     <i class="fa fa-star"></i> 
-                                     <i class="fa fa-star"></i> 
-                                     <i class="fa fa-star"></i> 
-                                     <i class="fa fa-star"></i>
-                                     <a href="#">(20 Review)</a>
-                                </span>
+                                
                                 <div class="days">
-                                    <i class="fa fa-clock-o"></i>
-                                    <a href="#">5 Days</a>
+                                    
+                                    
                                 </div>
                             </div>
                         </div>
